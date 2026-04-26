9d28c3c3cc07493d2382cd465c509219eaab635a
MD SAD MAUN SAMI <mdsadmaunsami@gmail.com>
2026-04-27 01:56:19 +0600

    Add program to concatenate first and last names

---
+++ first-last nameadd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char n1[100],n2[100];
+    printf("Enter first name: ");
+    scanf("%s",n1);
+    printf("Enter last name: ");
+    scanf("%s",n2);
+    printf("Full Name: %s %s\n",n1,n2);
+    return 0;
+}
